ef1c04263332dff10e9cea4a74542b20f3b927b1
Noah Kessler <njk40@cam.ac.uk>
2019-05-06 00:45:35 +0100

    Initial commit of Unity/Vuforia AR app - including all progress since beginning last night

---
+++ MessageAR2/Assets/Message.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Message : MonoBehaviour
+{
+    private OLocation location;
+    public string message;
+    public long id;
+    public Message(OLocation loc, string msg, long ID)
+    {
+        this.location = loc;
+        this.message = msg;
+        this.id = ID;
+    }
+
+    public string getMessage()
+    {
+        return this.message;
+    }
+
+    public OLocation getLoc()
+    {
+        return this.location;
+    }
+
+}
---
+++ MessageAR2/Assets/OLocation.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class OLocation {
+    public float lat, lon, alt;
+
+    public OLocation(float la, float lo, float a)
+    {
+        this.lat = la;
+        this.lon = lo;
+        this.alt = a;
+    }
+    
+    public Vector3 diff(OLocation other)
+    {
+        Vector3 rp = Vector3.zero;
+        rp.Set(this.lat - other.lat, this.lon - other.lon, this.alt - other.alt);
+        return rp;
+    }
+
+    public float flatDist(OLocation other)
+    {
+        return Mathf.Sqrt(Mathf.Pow(this.lat - other.lat, 2) + Mathf.Pow(this.lon - other.lon, 2));
+    }
+}
