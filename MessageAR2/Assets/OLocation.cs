30dc2d65cf1adff62d2cac532d07450ad37235b1
Noah Kessler <njk40@cam.ac.uk>
2019-05-06 03:33:51 +0100

    Made substantive changes - simulated walking for demo; made AR sprites as logo-covered cubes; added UI overlay for information about clickedplaces

--- MessageAR2/Assets/OLocation.cs
+++ MessageAR2/Assets/OLocation.cs
@@ -20,6 +20,21 @@ public class OLocation {
 
     public float flatDist(OLocation other)
     {
-        return Mathf.Sqrt(Mathf.Pow(this.lat - other.lat, 2) + Mathf.Pow(this.lon - other.lon, 2));
+        return Mathf.Sqrt(Mathf.Pow(this.lat - other.lat, 2) + Mathf.Pow(this.lon - other.lon, 2) + Mathf.Pow(this.alt - other.alt, 2));
+    }
+
+    public OLocation copy()
+    {
+        return new OLocation(lat, lon, alt);
+    }
+
+    public bool equals(OLocation other)
+    {
+        return this.lat == other.lat && this.lon == other.lon && this.alt == other.alt;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("{0:0.0000} {1:0.0000} {2:0.0000}", this.lat, this.lon, this.alt);
     }
 }
--- MessageAR2/Assets/TapObject.cs
+++ MessageAR2/Assets/TapObject.cs
@@ -1,33 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TapObject : MonoBehaviour {
     public GameObject metalBall;
     public GameObject flatBall;
+    public CanvasGroup uiCanvasGroup;
     private Dictionary<long, GameObject> messageBalls;
     private Dictionary<long, Message> messages;
-    private const float LOC_UPDATE_DELAY = 600;
-    private float distThreshold = 200;
+    //private const float LOC_UPDATE_DELAY = 50;
+    private float distThreshold = 5.0f;
     private bool updateLock = false;
 
     //private LocationInfo loc_origin;
+    private OLocation last_loc;
     private OLocation loc_origin;
 
     // Use this for initialization
-    void Start () {
+    void Start () { 
         // load with initialized location
+        // for demo purposes, use dummy locations
+        loc_origin = new OLocation(1, 1, 10);
+        last_loc = loc_origin.copy();
         // and update location periodically
         messages = new Dictionary<long, Message>();
         messageBalls = new Dictionary<long, GameObject>();
-        InvokeRepeating("UpdateLocation", 0, LOC_UPDATE_DELAY);
-	}
+        uiCanvasGroup.alpha = 0f;
+        uiCanvasGroup.blocksRaycasts = false;
+        //InvokeRepeating("UpdateLocation", 0, LOC_UPDATE_DELAY);
+    }
 
     void UpdateLocation()
     {
-        //loc_origin = xxx;
-        loc_origin = new OLocation(1, 1, 10);
-        print("Update location");
+        // get updated location
+        // dummy method to simulate walking
+        //loc_origin.lat *= 1.01f;
+        loc_origin.lon += 0.02f;
+        loc_origin.alt += 0.02f;
+        print("Last pos: " + last_loc.ToString());
+        print("Curr pos: " + loc_origin.ToString());
+
     }
 
     Message[] GetMessagesFromBlockchain()
@@ -36,10 +49,13 @@ public class TapObject : MonoBehaviour {
         // to find relatively nearby messages
 
         // NB: currently populated w dummy data
-        return new Message[] { new Message(new OLocation(1.301f, 1.001f, 13), "3m up", 101),
-                new Message(new OLocation(1.2f, 1.001f, 11), "1m up", 293),
-                 new Message(new OLocation(1.001f, 0.891f, 12), "2m up", 338),
-                new Message(new OLocation(0.998f, 1.001f, 10), "0m up", 928)
+        return new Message[] { new Message(new OLocation(1.4f, 1.2f, 14), "Stroll down Isaac Newton's Secret Passageway - just $0.05 for access info!", 101),
+                new Message(new OLocation(1.4f, 1.001f, 13), "Discount for all MetaVibe users on all products", 293),
+                 new Message(new OLocation(0.45f, 1.001f, 12), "Great place to sit and relax", 338),
+                new Message(new OLocation(1.55f, 1.001f, 11.5f), "Meet up here every Tuesday for free walking tour", 928),
+                new Message(new OLocation(1.55f, 1.001f, 15f), "Really rude waiters here - avoid!", 720),
+                new Message(new OLocation(1.55f, 1.001f, 16f), "Like my street art here? Leave me a small tip as a thanks!", 888),
+                 new Message(new OLocation(0.55f, 1.2f, 14f), "Good food", 123)
                 };
     }
 
@@ -52,6 +68,7 @@ public class TapObject : MonoBehaviour {
         
         foreach (Message m in messages.Values)
         {
+            print("message " + m.id + " dist " + m.getLoc().flatDist(loc_origin));
             if (m.getLoc().flatDist(loc_origin) > maxDist)
             {
                 toRemove.Add(m.id);
@@ -60,8 +77,8 @@ public class TapObject : MonoBehaviour {
 
         foreach (Message m in msgsFromBC)
         {
-            print("Message " + m.id + " dist " + m.getLoc().flatDist(loc_origin));
-            print("mess contains? " + messages.ContainsKey(m.id));
+            //print("Message " + m.id + " dist " + m.getLoc().flatDist(loc_origin));
+            //print("mess contains? " + messages.ContainsKey(m.id));
             if (!messages.ContainsKey(m.id) && !toRemove.Contains(m.id) && m.getLoc().flatDist(loc_origin) <= maxDist)
             {
                 toAdd.Add(m.id);
@@ -75,7 +92,7 @@ public class TapObject : MonoBehaviour {
             messages.Remove(mid);
         }
 
-        print("toAdd: " + toAdd.Count);
+        //print("toAdd: " + toAdd.Count);
         return new Dictionary<string, List<long>>()
         {
             {"add", toAdd},
@@ -94,45 +111,73 @@ public class TapObject : MonoBehaviour {
 
     void UpdateMessageObjects()
     {
-        print("UMO 0");
         Dictionary<string, List<long>> updates = UpdateNearbyMessages(distThreshold);
-        print("UMO 1");
         ClearMessageObjects(updates["remove"]);
 
-        this.transform.Find("helperText").GetComponent<TextMesh>().text = "" + messages.Count;
-        print("add: " + updates["add"].Count + " remove: " + updates["remove"].Count);
+        //this.transform.Find("helperText").GetComponent<TextMesh>().text = "" + messages.Count;
+        //print("add: " + updates["add"].Count + " remove: " + updates["remove"].Count);
         foreach (long mid in updates["add"])
         {
             print("adding message ball with id " + mid);
-            messageBalls[mid] = Instantiate(metalBall, messages[mid].getLoc().diff(loc_origin), Quaternion.identity);
+            messageBalls[mid] = Instantiate(metalBall, messages[mid].getLoc().diff(loc_origin), Quaternion.AngleAxis(180, Vector3.up));
             messageBalls[mid].AddComponent<Message>();
             messageBalls[mid].GetComponent<Message>().message = messages[mid].getMessage();
             messageBalls[mid].GetComponent<Message>().id = mid;
         }
 
+        
+        if (!last_loc.equals(loc_origin))
+        {
+            foreach (long mid in messageBalls.Keys)
+            {
+                messageBalls[mid].transform.position = messages[mid].getLoc().diff(loc_origin);
+            }
+            last_loc = loc_origin.copy();
+        }
+        
         updateLock = false;
     }
 
     // Update is called once per frame
     void Update() {
 
+
+        if (Time.frameCount % 100 == 0)
+        {
+            print(Time.frameCount);
+        }
+
+        if (Time.frameCount % 100 == 0)
+        {
+            UpdateLocation();
+        }
+
+        // update items every 100 frames
         if (Time.frameCount % 100 == 2 && !updateLock)
         {
             updateLock = true;
-            //UpdateNearbyMessages(distThreshold);
             UpdateMessageObjects();
         }
 
+        if (uiCanvasGroup.alpha > 0.1)
+        {
+            uiCanvasGroup.alpha -= 0.0015f;
+        }
+        else
+        {
+            uiCanvasGroup.alpha = 0f;
+        }
 
         // dummy activity below to check touch-interactivity is working
         if (Input.touchCount > 0)
         {
             Plane targetPlane = new Plane(transform.up, transform.position);
-            this.transform.Find("helperText").GetComponent<TextMesh>().text = "touch";
+            //this.transform.Find("helperText").GetComponent<TextMesh>().text = "touch";
 
             Touch touch = Input.touches[0];
             if (touch.phase == TouchPhase.Ended)
             {
+               
                 Ray ray = Camera.main.ScreenPointToRay(touch.position);
                 float dist = 0.0f;
                 targetPlane.Raycast(ray, out dist);
@@ -143,7 +188,9 @@ public class TapObject : MonoBehaviour {
                     //this.transform.Find("helperText").GetComponent<TextMesh>().text = "hit";
 
                     Transform pickedObject = hit.transform;
-                    this.transform.Find("helperText").GetComponent<TextMesh>().text = pickedObject.GetComponent<Message>().getMessage();
+                    //this.transform.Find("helperText").GetComponent<TextMesh>().text = "" + pickedObject.GetComponent<Message>().id;
+                    uiCanvasGroup.transform.Find("messageTextUI").GetComponent<Text>().text = pickedObject.GetComponent<Message>().message;
+                    uiCanvasGroup.alpha = 1f;
                     pickedObject.localScale = pickedObject.localScale * 0.9f;
                     print(Input.location.isEnabledByUser + " " + Input.location.status);
                 } else
