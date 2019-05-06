569cd58665b97863885aec392127350ff21aaae4
root <robin@kohze.com>
2019-05-06 07:23:04 +0100

    completed upload, updated creation page, addaded money button adapter

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
-
+import { WebView } from 'react-native';
+import { Image, View } from 'react-native';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: 'Wallet',
   };
 
   render() {
@@ -14,7 +15,13 @@ export default class SettingsScreen extends React.Component {
       <ScrollView style={styles.container}>
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
-        
+      <View>
+      <WebView
+        source={{uri: 'https://www.moneybutton.com'}}
+        style={{height: 820}}
+      /> 
+
+      </View>
       </ScrollView>
     );
   }
@@ -23,7 +30,7 @@ export default class SettingsScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
+    paddingTop: 0,
     backgroundColor: '#fff',
   },
 });
